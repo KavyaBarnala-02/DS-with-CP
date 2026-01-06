a026aafec568c1e82941056ff4542118ca0f4fbf
KavyaBarnala-02 <241814100002@cutmap.ac.in>
2026-01-06 12:27:57 +0530

    Add Node class with constructor and getdata method

--- Module1_Array/Task5.java
+++ Module1_Array/Task5.java
@@ -1,1 +1,25 @@
-node class
+class Node{
+    int data;
+    Node next;
+    //constructor
+    Node(int S){
+        data=S;
+        next=null;
+    }
+    //getdata
+    public void getdata(){
+        System.out.println(data);}
+}
+class Main {
+    public static void main(String[] args) {
+        System.out.println("SIRI");
+        Node S=new Node(4);
+        Node p=new Node(9);
+        
+        System.out.println(S);
+        S.getdata();
+        p.getdata();
+    
+        
+    }
+}
