97f876a9db9a2ef478e74ec4dce9a88db47b90d8
KavyaBarnala-02 <241814100002@cutmap.ac.in>
2026-01-20 11:38:19 +0530

    Refactor Node class and update main method output

--- Module1_Array/Task5.java
+++ Module1_Array/Task5.java
@@ -1,25 +1,63 @@
-class Node{
+class Node
+{
     int data;
     Node next;
-    //constructor
-    Node(int S){
-        data=S;
+    Node(int a)
+    {
+        data=a;
         next=null;
     }
-    //getdata
-    public void getdata(){
-        System.out.println(data);}
+    
+    // 
+    public void getData(){
+        
+        System.out.println(data);
+        
+    }
 }
 class Main {
     public static void main(String[] args) {
-        System.out.println("SIRI");
-        Node S=new Node(4);
-        Node p=new Node(9);
+        System.out.println("varshh");
         
-        System.out.println(S);
-        S.getdata();
-        p.getdata();
-    
+        Node a=new Node(10);
+        Node b=new Node(20);
+        Node c=new Node(30);
+        Node d=new Node(40);
+        Node e=new Node(50);
+        Node f=new Node(60);
         
+        a.data=100;// updation
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=f;
+        f.next=a;
+        
+        
+        System.out.println(a.data);
+        System.out.println(a.next);
+        
+        System.out.println(b.data);
+        System.out.println(b.next);
+        
+        System.out.println(c.data);
+        System.out.println(c.next);
+        
+        System.out.println(d.data);
+        System.out.println(d.next);
+        
+        System.out.println(e.data);
+        System.out.println(e.next);
+        System.out.println(f.data);
+        System.out.println(f.next);
+        
+        
+        a.getData();
+        b.getData();
+         c.getData();
+          d.getData();
+           e.getData();
+           f.getData();
     }
 }
